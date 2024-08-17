d1d9c97ae9004bfe19ea8d0a122777a70d5f5a7b
Kostas Minaidis <minaidisk@gmail.com>
2024-08-17 16:47:19 +0300

    Replace python dictionary with JS object terminology.

--- langchain/src/chains/combine_documents/stuff.ts
+++ langchain/src/chains/combine_documents/stuff.ts
@@ -27,10 +27,10 @@ import {
  * @param documentPrompt Prompt used for formatting each document into a string. Input
     variables can be "page_content" or any metadata keys that are in all documents.
     "page_content" will automatically retrieve the `Document.page_content`, and all 
-    other inputs variables will be automatically retrieved from the `Document.metadata` dictionary. Default to a prompt that only contains `Document.page_content`.
+    other inputs variables will be automatically retrieved from the `Document.metadata` object. Default to a prompt that only contains `Document.page_content`.
  * @param documentSeparator String separator to use between formatted document strings.
  * @returns An LCEL `Runnable` chain.
-    Expects a dictionary as input with a list of `Document`s being passed under
+    Expects a object as input with a list of `Document`s being passed under
     the "context" key.
     Return type depends on the `output_parser` used.
  */
